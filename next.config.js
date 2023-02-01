f39ad60488ccb61d8dd217f2335a55882f9aac04
lalo <jorgegil9706@gmail.com>
2023-02-01 17:07:16 -0500

    planets rapidapi working

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
+/* @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["d205bpvrqc9yn1.cloudfront.net"],
+    loader:"default",
+ },
 }
 
 module.exports = nextConfig
+
